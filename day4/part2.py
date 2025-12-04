f83e85f93f442ac6bdfdbbc233f7bfa3cf8da198
saniter <saniter.dev@outlook.com>
2025-12-05 00:55:17 +0800

    finish day4

---
+++ day4/part2.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+with open("./input.txt") as f:
+    papers = f.readlines()
+
+_matrix = [[c for c in papers[i].strip()] for i in range(len(papers))]
+matrix: np.ndarray = np.array(_matrix) == "@"
+height, width = matrix.shape
+
+
+def sub_matrix(x, y, r):
+    return matrix[
+        max(0, y - r) : min(height, y + r + 1),
+        max(0, x - r) : min(width, x + r + 1),
+    ]
+
+
+def check(x, y):
+    submatrix = sub_matrix(x, y, 1)
+    if np.sum(submatrix) <= 4:  # include self
+        return True
+    return False
+
+
+total = 0
+while True:
+    ys, xs = np.where(matrix)
+    removed = [(y, x) for x, y in zip(xs, ys) if check(x, y)]
+    if (added := len(removed)) == 0:
+        break
+    total += added
+    removed = np.transpose(removed)
+    matrix[*removed] = False
+
+print(f"totoal:{total}")
